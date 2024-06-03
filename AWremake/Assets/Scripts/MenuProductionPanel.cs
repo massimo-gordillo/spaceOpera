66babf454c4c6e34f82b161877b495f7e2b7bb51
MassimoGordillo <massimogordillo@hotmail.com>
2024-06-03 00:25:51 -0400

    ProductionMenu is certainly doing something... it's populating a very long list of Infantry and LightTank. I really hope my SO isn't continuously adding items to the AttributesBaseUnits every time I save the scene in Unity. That could overtake data availability quickly. Probably good to investigate. I have to tie the sprite declaration in GameValuesSO to the sprite reference in BaseUnit.

--- AWremake/Assets/Scripts/MenuProductionPanel.cs
+++ AWremake/Assets/Scripts/MenuProductionPanel.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class MenuProductionPanel : MonoBehaviour
 {
     public GameValuesSO gameValuesSO;
-    public GameObject productionButton;
+    public MenuProductionButton productionButton;
     public Transform productionContent;
     private List<AttributesBaseUnit> attributesBaseUnits;
     // Start is called before the first frame update
@@ -15,7 +15,11 @@ public class MenuProductionPanel : MonoBehaviour
         attributesBaseUnits = gameValuesSO.getAttributesBaseUnits();
         foreach (AttributesBaseUnit attributesBaseUnit in attributesBaseUnits)
         {
-            GameObject nextProductionButton = productionButton;
+            MenuProductionButton nextProductionButton = productionButton;
+            nextProductionButton.buttonText.text = attributesBaseUnit.unitName + "\n"+attributesBaseUnit.price;
+            nextProductionButton.sprite.sprite = attributesBaseUnit.sprite;
+            //I want to set the size of the sprite ahead of time. Maybe I should do this at startup as well??
+            //nextProductionButton.sprite.GetComponent<RectTransform>().sizeDelta = new Vector2(5, 5);
             Instantiate(nextProductionButton, new Vector2(0, 0), Quaternion.identity, productionContent);
         }
     }
