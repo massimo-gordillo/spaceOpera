aec20d330cbe52d1754b53e81ac5d1da9d85cbb4
MassimoGordillo <massimogordillo@hotmail.com>
2024-06-03 18:09:36 -0400

    Previous commit expanded MenuProductionButton to record what prefab it refers to upon its own instantiation from MenuProductionPanel. MenuProductionButton will now pass the prefab its paired with to GameMaster for an instance of that BaseUnit prefab to be created. Requires debugging.

--- AWremake/Assets/Scripts/MenuProductionButton.cs
+++ AWremake/Assets/Scripts/MenuProductionButton.cs
@@ -9,4 +9,17 @@ public class MenuProductionButton : MonoBehaviour
     public TMP_Text buttonText;
     public SpriteRenderer sprite;
     public GameObject unitPrefab;
+    public GameMaster gameMaster;
+
+    public void Start()
+    {
+        gameMaster = GameObject.FindGameObjectWithTag("GameMasterTag").GetComponent<GameMaster>();
+
+    }
+
+    public void OnClick()
+    {
+        GameObject tempInstance = Object.Instantiate(unitPrefab);
+        gameMaster.unitProductionButtonPressed(tempInstance.GetComponent<BaseUnit>());
+    }
 }
